7edbb4e655f02ed8d26de89d818a129343449b54
Min Guo <xmguomin@gmail.com>
2015-09-19 15:47:26 -0400

    Readfile works

--- src/BFS.java
+++ src/BFS.java
@@ -1,6 +1,39 @@
 /**
  * Created by Min on 9/19/15.
  */
+import java.io.*;
+
 public class BFS {
-    
+    int taskNumber = 0;
+    int targetValue = 0;
+    int deadline = 0;
+    int queueSize = 0;
+
+    public  static void readFile(String fileName) {
+        String line = null;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            line = bufferedReader.readLine();
+            System.out.println(line);
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+        }
+    }
+
+    public static void main (String [] args) {
+        String fileName = null;
+        if(args.length > 0) {
+            fileName = args[0];
+        }
+        readFile(fileName);
+    }
 }
