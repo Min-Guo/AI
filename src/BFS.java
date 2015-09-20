e9da093a0167b27a555cc44f35cc713322e2e761
Min Guo <xmguomin@gmail.com>
2015-09-20 00:37:30 -0400

    Read data into nodeStructure

--- src/BFS.java
+++ src/BFS.java
@@ -2,19 +2,52 @@
  * Created by Min on 9/19/15.
  */
 import java.io.*;
+import java.util.ArrayList;
 
 public class BFS {
-    int taskNumber = 0;
-    int targetValue = 0;
-    int deadline = 0;
-    int queueSize = 0;
+    public static int taskNumber = 0;
+    public static int targetValue = 0;
+    public static int deadline = 0;
+    public static int queueSize = 0;
+
+    public class NodeStructure {
+        private int value;
+        private int time;
+
+        public void setValue (int value) {
+            this.value = value;
+        }
+
+        public void setTime (int time) {
+            this.time = time;
+        }
+
+    }
+
 
     public  static void readFile(String fileName) {
+        ArrayList<NodeStructure> nodeStructureList = new ArrayList<NodeStructure>();
         String line = null;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
             line = bufferedReader.readLine();
-            System.out.println(line);
+            String[] splited = line.split("\\s+");
+            taskNumber = Integer.parseInt(splited[0]);
+            targetValue = Integer.parseInt(splited[1]);
+            deadline = Integer.parseInt(splited[2]);
+            queueSize = Integer.parseInt(splited[3]);
+            for (int i = 0; i < taskNumber; i++) {
+                line = bufferedReader.readLine();
+                splited = line.split("\\s+");
+                BFS bfs = new BFS();
+                NodeStructure temp = bfs.new NodeStructure();
+                temp.setValue(Integer.parseInt(splited[0]));
+                temp.setTime(Integer.parseInt(splited[1]));
+                nodeStructureList.add(temp);
+            }
+            for(NodeStructure nodeStructure :nodeStructureList) {
+                System.out.println("value is " + nodeStructure.value + ", time is " + nodeStructure.time);
+            }
             bufferedReader.close();
         }
         catch(FileNotFoundException ex) {
