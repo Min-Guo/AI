95551796bfa35625de2bc2ba60f2e8723c38bab8
Min Guo <xmguomin@gmail.com>
2015-10-14 02:11:51 -0400

    Add Problem Set3

--- Assignment2/src/DPLLSearch.java
+++ Assignment2/src/DPLLSearch.java
@@ -2,4 +2,13 @@
  * Created by Min on 10/4/15.
  */
 public class DPLLSearch {
+    public static void readClause (){
+
+    	System.out.println("Hello world!");
+    }
+
+    public static void main (String [] args) {
+        ArrayList<ArrayList<String>> clauseSet = new ArrayList<ArrayList<String>> ();
+        readClause();
+    }
 }
